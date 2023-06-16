8fa9de59da69912e76301cfab580b8a2d580425d
Nook <nookittipon@gmail.com>
2023-06-17 01:55:59 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+    return (
+        <div
+            style={{
+                padding: 50,
+                textAlign: 'center',
+            }}
+        >
+            <h1>Start Here</h1>
+        </div>
+    )
+}
+
+export default App
